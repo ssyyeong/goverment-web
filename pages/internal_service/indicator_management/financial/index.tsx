87103cdd4e421909c21b0bdbdeffb1e8a45fcd31
leanoncompany <90431948+leanoncompany@users.noreply.github.com>
2024-03-11 15:33:03 +0900

    fix: 최근 6개월에서 요번달 제외

--- pages/internal_service/indicator_management/financial/index.tsx
+++ pages/internal_service/indicator_management/financial/index.tsx
@@ -113,12 +113,12 @@ const Page: NextPage = () => {
 	 *
 	 */
 	const categories = [
+		today.subtract(7, 'month').format('YYYY-MM'),
+		today.subtract(6, 'month').format('YYYY-MM'),
 		today.subtract(5, 'month').format('YYYY-MM'),
 		today.subtract(4, 'month').format('YYYY-MM'),
 		today.subtract(3, 'month').format('YYYY-MM'),
 		today.subtract(2, 'month').format('YYYY-MM'),
-		today.subtract(1, 'month').format('YYYY-MM'),
-		today.format('YYYY-MM'),
 	];
 
 	const colorChip = [
@@ -1117,7 +1117,7 @@ const Page: NextPage = () => {
 													textAlign={'center'}
 													mb={7}
 												>
-													카테고리별 비율
+													카테고리별 총 비율
 												</Typography>
 												<ReactApexChart
 													type="pie"
@@ -1206,7 +1206,7 @@ const Page: NextPage = () => {
 													textAlign={'center'}
 													mb={7}
 												>
-													카테고리별 비율
+													카테고리별 총 비율
 												</Typography>
 												<ReactApexChart
 													type="pie"
