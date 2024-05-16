79238e2ec665ee005fe8b6ed4d292c19780e1c68
yeong <133121353+ssyyeong@users.noreply.github.com>
2024-05-16 11:49:21 +0900

    메인 페이지 변경중

--- pages/introduction/supporti/index.tsx
+++ pages/introduction/supporti/index.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import { NextPage } from 'next';
 
-import { Box } from '@mui/material';
+import {
+	Box,
+	ToggleButton,
+	ToggleButtonGroup,
+	Typography,
+} from '@mui/material';
+import SupportiTab from '../../../src/views/global/SupportiTab';
 
 const Page: NextPage = () => {
+	//* States
+	const [tab, setTab] = React.useState('ko');
 	//* Modules
 	//* Constants
 	//* Hooks
 
-	return <Box></Box>;
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+				width: '100%',
+				px: { sm: 5, xs: 2 },
+			}}
+		>
+			<SupportiTab
+				tabList={['ko', 'en']}
+				setValue={setTab}
+				value={tab}
+				tabContentList={[
+					<Box>
+						<Typography variant="h2">한국어</Typography>
+					</Box>,
+					<Box>
+						<Typography variant="h2">English</Typography>
+					</Box>,
+				]}
+			/>
+		</Box>
+	);
 };
 
 export default Page;
